25008b47dbd034c5587c42c6722d90762b198d9d
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-01-24 21:47:15 +0000

    first commit: ec2

---
+++ ec2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# create ec2 instance
+resource "aws_instance" "example" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.security_group_ids
+
+  tags = {
+    Name = "my-web-server"
+  }
+
+}
