c68b184f621b9864922d3739be2563be430a5af9
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-02-17 21:05:29 +0000

    added eks

---
+++ eks/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# create eks cluster
+resource "aws_eks_cluster" "demo" {
+  name = "demo-eks-cluster"
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = var.eks_cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnets
+  }
+
+  tags = {
+    Name = "demo-eks-cluster"
+  }
+}
+
+
+resource "aws_eks_node_group" "demo" {
+  cluster_name    = aws_eks_cluster.demo.name
+  node_group_name = "demo-node-group"
+  node_role_arn   = var.node_group_role_arn
+  subnet_ids      = var.subnets    
+  instance_types = ["t2.micro"] # optional, default is "t3.medium"
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
