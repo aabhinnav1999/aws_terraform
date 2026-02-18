3a50d16350e77f894eb8edaf19287d7d42135228
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-02-18 23:12:09 +0000

    updated

--- eks/main.tf
+++ eks/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 # create eks cluster
 resource "aws_eks_cluster" "demo" {
-  name = "demo-eks-cluster"
+  name = "tf-eks-cluster"
 
   access_config {
     authentication_mode = "API"
@@ -26,14 +26,14 @@ resource "aws_eks_cluster" "demo" {
   }
 
   tags = {
-    Name = "demo-eks-cluster"
+    Name = "tf-eks-cluster"
   }
 }
 
 
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "demo-node-group"
+  node_group_name = "tf-node-group"
   node_role_arn   = var.node_group_role_arn
   subnet_ids      = var.subnets    
   instance_types = ["t2.micro"] # optional, default is "t3.medium"
