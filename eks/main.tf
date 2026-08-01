08c8e73218563e1cf6c692541b400c3467c5f91b
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-08-01 13:15:56 +0100

    added pod identity

--- eks/main.tf
+++ eks/main.tf
@@ -178,3 +178,46 @@ resource "aws_eks_access_policy_association" "access_policy_association_2" {
     type       = "cluster"
   }
 }
+
+# resource "aws_eks_addon" "pod_identity_agent" {
+#   cluster_name = aws_eks_cluster.demo.name
+#   addon_name   = "eks-pod-identity-agent"
+# }
+
+# data "aws_iam_policy_document" "ebs_csi_pod_identity_trust" {
+#   statement {
+#     effect  = "Allow"
+#     actions = ["sts:AssumeRole", "sts:TagSession"]
+
+#     principals {
+#       type        = "Service"
+#       identifiers = ["pods.eks.amazonaws.com"]
+#     }
+#   }
+# }
+
+# resource "aws_iam_role" "ebs_csi_pod_identity" {
+#   name               = "${aws_eks_cluster.demo.name}-ebs-csi-driver-role-podidentity"
+#   assume_role_policy = data.aws_iam_policy_document.ebs_csi_pod_identity_trust.json
+# }
+
+# resource "aws_iam_role_policy_attachment" "ebs_csi_pod_identity" {
+#   role       = aws_iam_role.ebs_csi_pod_identity.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+# }
+
+# resource "aws_eks_addon" "ebs_csi_pod_identity" {
+#   cluster_name = aws_eks_cluster.demo.name
+#   addon_name   = "aws-ebs-csi-driver"
+
+#   depends_on = [aws_eks_addon.pod_identity_agent]
+# }
+
+# resource "aws_eks_pod_identity_association" "ebs_csi" {
+#   cluster_name    = aws_eks_cluster.demo.name
+#   namespace       = "kube-system"
+#   service_account = "ebs-csi-controller-sa"
+#   role_arn        = aws_iam_role.ebs_csi_pod_identity.arn
+
+#   depends_on = [aws_eks_addon.ebs_csi_pod_identity]
+# }
