bd635630aeaedfc923bb00cf45048d1f67554807
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-08-21 21:33:11 +0100

    added multiple instances config

--- ec2/main.tf
+++ ec2/main.tf
@@ -9,55 +9,55 @@ terraform {
 
 provider "aws" {
   region = "eu-west-1"
-  profile = "ec2-profile"
+  # profile = "ec2-profile"
 }
 
 # create ec2 instance
-resource "aws_instance" "example" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  vpc_security_group_ids = var.security_group_ids
+# resource "aws_instance" "example" {
+#   ami                    = var.ami_id
+#   instance_type          = var.instance_type
+#   key_name               = var.key_name
+#   vpc_security_group_ids = var.security_group_ids
 
-  tags = {
-    Name = "my-web-server"
-  }
+#   tags = {
+#     Name = "my-web-server"
+#   }
 
-  root_block_device {
-    volume_size = 20
-    volume_type = "gp3"
-  }
+#   root_block_device {
+#     volume_size = 20
+#     volume_type = "gp3"
+#   }
 
-  # provisioner "file" {
-  #   source      = "docker.sh"
-  #   destination = "/tmp/docker.sh"
+# provisioner "file" {
+#   source      = "docker.sh"
+#   destination = "/tmp/docker.sh"
 
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "ubuntu"
-  #     private_key = file(var.private_key_path)
-  #     host        = self.public_ip  
-  # }
-  # }
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"
+#     private_key = file(var.private_key_path)
+#     host        = self.public_ip  
+# }
+# }
 
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "chmod +x /tmp/docker.sh",
-  #     "sudo /tmp/docker.sh"
-  #   ]
+# provisioner "remote-exec" {
+#   inline = [
+#     "chmod +x /tmp/docker.sh",
+#     "sudo /tmp/docker.sh"
+#   ]
 
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "ubuntu"
-  #     private_key = file(var.private_key_path)
-  #     host        = self.public_ip
-  #   } 
-  # }
-}
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"
+#     private_key = file(var.private_key_path)
+#     host        = self.public_ip
+#   } 
+# }
+# }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-}
+# output "public_ip" {
+#   value = aws_instance.example.public_ip
+# }
 
 # create efs
 # resource "aws_efs_file_system" "example" {
@@ -70,19 +70,20 @@ output "public_ip" {
 # }
 
 # create multiple ec2 instances
-# resource "aws_instance" "example_multiple" {
-#   for_each = toset([1, 2, 3])
-#   ami                    = var.ami_id
-#   instance_type          = var.instance_type
-#   key_name               = var.key_name
-#   vpc_security_group_ids = var.security_group_ids
+resource "aws_instance" "example_multiple" {
 
-#   tags = {
-#     Name = "my-web-server-${each.key}"
-#   }
+  count                  = 90
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = var.security_group_ids
 
-#   root_block_device {
-#     volume_size = 10
-#     volume_type = "gp3"
-#   }
-# }
+  tags = {
+    Name = "my-server-${format("%02d", count.index + 1)}"
+  }
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp3"
+  }
+}
